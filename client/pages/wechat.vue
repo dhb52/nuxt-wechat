df78a17228b41760f419270ae4c69636769c04ad
dhb52 <dhb52@126.com>
2019-02-18 13:46:41 +0800

    demo page

---
+++ client/pages/wechat.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <img :src="wechatUser.avatar">
+    {{ wechatUser.name }}
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    wechatUser() {
+      return this.$store.state.wechat.user
+    }
+  },
+
+  mounted() {
+    this.$wechatShare(
+      'Barry',
+      'Nuxtjs Wechat',
+      'http://thirdwx.qlogo.cn/mmopen/vi_32/hJGFkv2NPy1JW1mvuibKO4c6Oln49clZHHcapD0OxLzTt3ufqiaQxyOUf4xGFgPvy9EzTNyw25qFAPSK8ESKhlag/132'
+    )
+  },
+
+  middleware: ['wechatAuth']
+}
+</script>
+
+<style>
+</style>
